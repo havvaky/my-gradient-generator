7f04085c95e9555ca2b49cf70639b5a54708af7f
havvak <havvak@yahoo.com>
2021-06-02 10:26:09 -0400

    clipboard

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 
 import styles from '../styles/Home.module.scss';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import BrushIcon from '@material-ui/icons/Brush';
+import copy from "copy-to-clipboard";
+
 
 
 export default function Home() {
@@ -9,13 +11,6 @@ export default function Home() {
   const [color1, setColor1] = useState("#04cdca");
   const [color2, setColor2] = useState("#d763f8");
 
-  const handleColor1 = e => {
-    setColor1(e.target.value);
-  }
-
-  const handleColor2 = e => {
-    setColor2(e.target.value);
-  }
 
   const handleRandomColor = () => {
     let randomColor1 = Math.random().toString(16).slice(2, 8);
@@ -25,13 +20,17 @@ export default function Home() {
     setColor2(`#${randomColor2}`);
   }
 
+  const handleCopyClipboard = () => {
+    copy(`background: linear-gradient(to right, ${color1}, ${color2})`);
+  }
+
   return (
     <>
       <div className={styles.container}>
         <main className={styles.main}
               style={{ background: `linear-gradient(to right, ${color1}, ${color2})`, transition: 'all 1s' }}>
           <div className={styles.wrapper}>
-            <h1>CSS Gradient Color Generator</h1>
+            <h1 className={styles.title}>CSS Gradient Color Generator</h1>
             <div className={styles.colorContainer}>
               <div className={styles.color} style={{ background: `${color1}` }}>
                 <div className={styles.colorLabel}><code>{color1}</code></div>
@@ -40,7 +39,7 @@ export default function Home() {
                   id="color1"
                   className={styles.colorPicker}
                   value={color1}
-                  onChange={handleColor1}/>
+                  onChange={e => setColor1(e.target.value)}/>
                 <div className={styles.brush}><BrushIcon/></div>
               </div>
               <div className={styles.color} style={{ background: `${color2}` }}>
@@ -50,12 +49,13 @@ export default function Home() {
                   id="color2"
                   className={styles.colorPicker}
                   value={color1}
-                  onChange={handleColor2}/>
+                  onChange={e => setColor2(e.target.value)}/>
                 <div className={styles.brush}><BrushIcon/></div>
               </div>
             </div>
             <button className={styles.randomColorButton} onClick={handleRandomColor}>Generate Random Color</button>
-            <p className={styles.code}><code>{`background: linear-gradient(to right, ${color1}, ${color2})`}</code></p>
+            <p className={styles.codeText}><code>{`background: linear-gradient(to right, ${color1}, ${color2})`}</code></p>
+            <button className={styles.copyButton} onClick={handleCopyClipboard}>Copy to Clipboard</button>
           </div>
         </main>
       </div>
