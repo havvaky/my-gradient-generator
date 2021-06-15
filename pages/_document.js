79a7f1263acaf8f83b4b8f3fdacc06a133fe388c
havvak <havvak@yahoo.com>
2021-06-15 14:50:22 -0400

    uninstall package

--- pages/_document.js
+++ pages/_document.js
@@ -13,8 +13,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200;1,200&family=Varela+Round&display=swap" rel="stylesheet"/>
-          <link
-            href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet"/>
           <title>My Gradient Generator</title>
         </Head>
         <body>
